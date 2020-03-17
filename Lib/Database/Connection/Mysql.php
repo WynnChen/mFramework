1dddb13e4c5d39fcd284d486cb2cb074cc095919
wynn <wynn.chen.cn@gmail.com>
2020-03-17 18:34:27 +0800

    移除强制执行 set names = utf8

--- Lib/Database/Connection/Mysql.php
+++ Lib/Database/Connection/Mysql.php
@@ -17,7 +17,7 @@ namespace mFramework\Database\Connection;
  * 'username' => 用户名
  * 'password' => 密码
  * 'charset' => 字符集，PHP程序这端使用的，不是Mysql数据库内的。一般utf8
- * 'options' => array, 配置选项。PDO::MYSQL_ATTR_INIT_COMMAND无需配置，强制执行。
+ * 'options' => array, 配置选项。
  *
  * 注意：没有执行setAttribute(PDO::ATTR_EMULATE_PREPARES, false)，由外部程序自行决定。
  * 
@@ -32,7 +32,7 @@ class Mysql extends \mFramework\Database\Connection
 		// 建立数据库连接
 		$options = $config['options'];
 		$options[\PDO::ATTR_ERRMODE] = \PDO::ERRMODE_EXCEPTION;
-		$options[\PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES {$config['charset']}";
+		//比较新的系统上似乎已经不需要了？ $options[\PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES {$config['charset']}"; 
 		// charset需要PHP5.3.6及以上。
 		parent::__construct("mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}", $config['username'], $config['password'], $options);
 		// $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); 放到外部进行。
